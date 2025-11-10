1c71fb092df523b7903306b8a8a2723b3557cca5
Samuel McFadden <McFadden-S11@ulster.ac.uk>
2025-11-10 15:56:07 +0000

    Completed user class with private properties and public methods. Added parameterised constructor and login, logout and ControlDevice methods.

--- smartHomeController/User.cs
+++ smartHomeController/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,88 @@ namespace smartHomeController
 {
     internal class User
     {
+        //Private fields
+        private int userId;
+        private string userName;
+        private string password;
+        private string contactinfo;
+        private bool isLoggedIn;
+
+        //Public properties
+        public int UserId
+        {
+            get { return userId; }
+            set { userId = value; }
+        }
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        public string ContactInfo
+        {
+            get { return contactinfo; }
+            set { contactinfo = value; }
+        }
+        public bool IsLoggedIn
+        {
+            get { return isLoggedIn; }
+            set { isLoggedIn = value; }
+        }
+
+        public bool Login(string username, string password)
+        {
+            if (UserName == username && Password == password)
+            {
+                isLoggedIn = true;
+                Console.WriteLine("Login successful.");
+            }
+            else
+            {
+                isLoggedIn = false;
+                Console.WriteLine($"Login failed. Invalid username {username} or password.");
+            }
+            return isLoggedIn;
+        }
+
+        public void Logout()
+        {
+            if (IsLoggedIn)
+            {
+                IsLoggedIn = false;
+                Console.WriteLine("User logged out successfully.");
+            }
+            else
+            {
+                Console.WriteLine("User is not logged in.");
+            }
+        }
+
+        public void ControlDevice(int deviceID, string deviceName, string deviceAction)
+        {
+            if (isLoggedIn)
+            {
+                Console.WriteLine($"Preforming action {deviceAction} on {deviceName}");
+            }
+            else
+            {
+                Console.WriteLine("Please log in to control the device");
+            }
+
+
+        }
+        public User(int userID, string userName, string password, string contactinfo)
+        {
+            this.userId = userID;
+            this.userName = userName;
+            this.password = password;
+            this.contactinfo = contactinfo;
+            this.isLoggedIn = false;
+        }
     }
 }
